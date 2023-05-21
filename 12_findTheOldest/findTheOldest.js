56765ff91bbe816ae8eb41e35d0e73261cc29777
Ben <756858313@qq.com>
2023-05-21 12:39:56 +0800

    Finish this project

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return oldestAge < personAge ? person : oldest;
+    });
 };
 
+const getAge = function(yearOfBirth, yearOfDeath) {
+    if(!yearOfDeath) {
+        return new Date().getFullYear() - yearOfBirth;
+    }
+    return yearOfDeath - yearOfBirth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
